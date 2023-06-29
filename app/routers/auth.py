44567d5d5fbac9e7abbefac70fd74f0de8cb7a7d
Okite Chimaobi <okitesamuel28@gmail.com>
2023-06-29 20:30:50 +0100

    updated login

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -8,9 +8,9 @@ router = APIRouter(tags=['Authentication'])
 
 
 @router.post('/login', response_model=schemas.Token)
-def login(user_credentials: schemas.UserCreate, db: Session = Depends(database.get_db)):
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
     user = db.query(models.User).filter(
-        models.User.email == user_credentials.email).first()
+        models.User.email == user_credentials.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials")
     if not utils.verify(user_credentials.password, user.password):
