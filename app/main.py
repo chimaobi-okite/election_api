9e10f515b63399d6ebef532c163440ee56e1e7da
Okite Chimaobi <okitesamuel28@gmail.com>
2023-06-30 01:19:17 +0100

    updated cors

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ origins = [
     "http://localhost",
     "http://localhost:8080",
     "http://localhost:3000",
-    "https://election-manager.vercel.app/"
+    "https://election-manager.vercel.app"
 ]
 
 middleware = [
